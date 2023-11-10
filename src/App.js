7882e6fa31fc4902f8e5a48e9fffec6a7a6d7f0f
unknown <150411224+afterdark2587@users.noreply.github.com>
2023-11-10 11:56:45 +0700

    'up'

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          นายเจษฎาภรณ์   จิตรวิเศษ
+          chetsadaphon chitrwiset
         </p>
         <a
           className="App-link"
